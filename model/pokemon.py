0baac188d3a9aaf9a30f834b3e2674a65c730f06
thal01veloso <thal01veloso@gmail.com>
2021-06-02 11:31:51 -0300

    Adiciona os tipos

--- model/pokemon.py
+++ model/pokemon.py
@@ -1,5 +1,7 @@
 class Pokemon:
-    def __init__(self,nome,foto):
+    def __init__(self,nome,foto,type1,type2):
         self.nome=nome
         self.foto=foto
+        self.type2=type2
+        self.type1=type1
        
--- pokedex.py
+++ pokedex.py
@@ -12,15 +12,24 @@ def index():
 
 @app.route("/buscar", methods=['GET',"POST"])
 def buscar():
-    pokemon = Pokemon(request.form['nome'].lower(),"")
+    pokemon = Pokemon(request.form['nome'].lower(),"","","")
     try:
-        res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon.nome}").text
-        res = json.loads(res)['sprites']
-        res = res['front_default']
-        pokemon.foto=res
+        res = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon.nome}").text)
+        result = res['sprites']
+        result = result['front_default']
+        pokemon.foto=result
+        if len(res['types'])==2:
+            pokemon.type1=res['types'][0]['type']['name']
+            pokemon.type2=res['types'][1]['type']['name']
+        else:
+            pokemon.type1=res['types'][0]['type']['name']
     except:
         return "Nome incorreto"
-    return render_template("index.html", foto=pokemon.foto, nome=pokemon.nome.upper())
+    return render_template("index.html",
+    foto=pokemon.foto,
+    nome=pokemon.nome.upper(),
+    tipo1=pokemon.type1.upper(),
+    tipo2=pokemon.type2.upper())
 
 app.run(debug=True)
 
